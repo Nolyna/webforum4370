821664db199434b9e8d7bec514ecde30bcd2a242
hanankwok532 <hanankwok532@gmail.com>
2017-11-01 16:43:05 -0400

    Updated usersignup.php to include PDO

--- pages/usersignup.php
+++ pages/usersignup.php
@@ -1,18 +1,17 @@
 <?php
-	function usersignup() {
-		mysql_connect("","","") or die(mysql_error());
-		mysql_select_db("csc4370forum") or die(mysql_error());
+	$db = new PDO("mysql:host=localhost;dbname=csc4370forum", "root", "");
+	function usersignup($db) {	
 		$username = $_POST["username"];
 		$email = $_POST["email"];
 		$password = $_POST["password"];
 		$firstname = $_POST["firstname"];
 		$lastname = $_POST["lastname"];
-		$query = mysql_query("SELECT * FROM user WHERE name =".$username."or email=".$email."") or die(mysql_error());
-		$num = mysql_fetch_row($query);
+		$stmt = $db->query("SELECT * FROM user WHERE name =".$username."or email=".$email."");
+		/*$num = mysql_fetch_row($query);*/
 		if($num > 0){
 			$message = "The username name" .$name. "or email" .$email. "already exists.";
 		} else {
-			$in = mysql_query("INSERT INTO user VALUES(".$username.",".$password.", ".$email.", ".$firstname.",".$lastname.")") or die(mysql_error());
+			$in = $db->query("INSERT INTO user VALUES(".$username.",".$password.", ".$email.", ".$firstname.",".$lastname.")");
 			if($in){
 				$message = "Registration successful.";
 			} else {
