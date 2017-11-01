e0f2196ea289f291b9ef8c2711ea149b7fee99b8
hanankwok532 <hanankwok532@gmail.com>
2017-11-01 16:20:12 -0400

    Updated usersignup.php

--- pages/usersignup.php
+++ pages/usersignup.php
@@ -7,12 +7,12 @@
 		$password = $_POST["password"];
 		$firstname = $_POST["firstname"];
 		$lastname = $_POST["lastname"];
-		$query = mysql_query("SELECT * FROM user WHERE name ="'.$username.'"or email="'.$email.'"") or die(mysql_error());
+		$query = mysql_query("SELECT * FROM user WHERE name =".$username."or email=".$email."") or die(mysql_error());
 		$num = mysql_fetch_row($query);
 		if($num > 0){
-			$message = "The username name '.$name.' or email '.$email.' already exists.";
+			$message = "The username name" .$name. "or email" .$email. "already exists.";
 		} else {
-			$in = mysql_query("INSERT INTO user VALUES("'.$username.'","'.$password.'", "'.$email.'", "'.$firstname.'","'.$lastname.'")") or die(mysql_error());
+			$in = mysql_query("INSERT INTO user VALUES(".$username.",".$password.", ".$email.", ".$firstname.",".$lastname.")") or die(mysql_error());
 			if($in){
 				$message = "Registration successful.";
 			} else {
