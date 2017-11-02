9e1b275bc1ca25d554c1c7e8e450b42759122d14
Nolyna <Nolyna@users.noreply.github.com>
2017-11-02 09:38:04 -0400

    Add files via upload

---
+++ pages/setting_tab.php
@@ -0,0 +1,28 @@
+<h1>  Subscribe and Unsubscribe Notifications </h1>
+
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Notification preferences</th>
+      <th scope="col">Email</th>
+      <th scope="col">Phone</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td> New comment on post</td>
+      <td> Yes</td>
+      <td> Yes </td>
+    </tr>
+    <tr>
+      <td> New like on post</td>
+      <td> Yes</td>
+      <td> No </td>
+    </tr>
+    <tr>
+      <td>Newsletter</td>
+      <td> Yes</td>
+      <td> No </td>
+    </tr>
+  </tbody>
+</table>
