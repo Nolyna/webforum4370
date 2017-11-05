9c6cd5d8134e427a4a957fef4998576c8ff4150c
hanankwok532 <hanankwok532@gmail.com>
2017-11-05 12:06:02 -0500

    Updated overview_tab.php

--- pages/overview_tab.php
+++ pages/overview_tab.php
@@ -1,7 +1,24 @@
 <div class="row">
 	<div class="col-md-7">
+		<h2>Messages</h2>
+			<div>
+				
+			</div>
+		<h2>Users</h2>
+			<div>
+				
+			</div>
+		<h2>Statistics</h2>
+			<div>
+				
+			</div>
 		<?php
-			
+			/*
+				things that could be included in overview:
+				messages
+				list of non-admin users
+				forum statistics
+			*/
 		?>
 	</div>
 </div>
