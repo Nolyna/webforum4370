4de902239e0f797f1cf85688ec0ea4db33bb5587
hanankwok532 <hanankwok532@gmail.com>
2017-11-05 20:37:59 -0500

    Updated messages_tab.php

--- pages/messages_tab.php
+++ pages/messages_tab.php
@@ -1,5 +1,33 @@
 <div class="row">
 	<div class="col-md-7">
+		<h2>Messages</h2>
+		<?php
+			$messages = array_filter(getAllMessages());
+			if (empty($messages)) {
+				echo '<div class="jumbotron jumbotron-fluid">
+						<div class="container">
+							<p class="lead">No messages yet.</p>
+						</div>
+					  </div>';
+			}else{
+				echo '<table class="table">
+				  <thead>
+					<tr>
+					  <th scope="col">Email</th>
+					  <th scope="col">Message</th>
+					</tr>
+				  </thead>
+				  <tbody>' ;
+				foreach ($messages as $i => $messages) {
+					echo '
+					  <tr">
+						<td>'.$messages["email"].'</td>
+						<td>'.$messages["messageText"].'</td>
+					  </tr>';
+				}
+				echo' </tbody></table>';
+			}
+		?>
 		<form action = "" method = "post">
 			<h2>Send a Message</h2>
 			<div class="form-group">
